5065295fca6fdcbb5f7da246acea6313aa31fbb1
Cosimo Urselli <urselli.workmail@gmail.com>
2025-11-17 00:35:08 +0100

    auth jwt

--- settings/settings.py
+++ settings/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'rest_framework',
+    'rest_framework_simplejwt',
+    
     'apps.utenti',
     'apps.dipendenti',
     'apps.assenze',
@@ -129,3 +132,41 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication', 
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=10), 
+    
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7), 
+    
+    'ROTATE_REFRESH_TOKENS': True,
+    
+    'BLACKLIST_AFTER_ROTATION': True,
+
+    'ALGORITHM': 'HS256',
+    
+    'SIGNING_KEY': SECRET_KEY, 
+    
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    
+    'USER_ID_FIELD': 'id',
+    
+    'USER_ID_CLAIM': 'user_id',
+    
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'JTI_CLAIM': 'jti',
+}
--- settings/urls.py
+++ settings/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Endpoint JWT per l'autenticazione
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # APP URLs
+    path('api/utenti/', include('apps.utenti.urls')),
+    path('api/dipendenti/', include('apps.dipendenti.urls')),
+    path('api/contratti/', include('apps.contratti.urls')),
+    path('api/assenze/', include('apps.assenze.urls')),
 ]
